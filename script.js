58ec21c2e0780cc85a438f8f115c6585f1257fc7
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2023-08-20 02:19:22 +0200

    Add grid to container

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
 const container = document.querySelector("#container");
-const button = document.querySelector("#new-grid");
+const newGridButton = document.querySelector("#new-grid");
 
-// button.addEventListener("click", () => {
-//     makeRows(newGrid());
-// })
-
-function newGrid() {
-    let gridDimension = 0;
-    while (gridDimension <= 0 || gridDimension > 100) {
-        gridDimension = prompt("Select a new grid dimension (max 100).", 0);
-    }
-    makeRows(gridDimension);
-}
-
-function makeRows(rowNumber) {
-    for (let i = 0; i < rowNumber; i++) {
-        let newRow = document.createElement("div");
-        newRow.classList.add("row")
-        for (let j = 0; j < rowNumber; j++) {
-            let newColumn = document.createElement("div");
-            newColumn.classList.add("cell")
-            newRow.appendChild(newColumn);
-        }
-        container.appendChild(newRow);
+function makeGrid(dimension) {
+    container.style.setProperty("--grid-columns", dimension);
+    container.style.setProperty('--grid-rows', dimension);
+    for (let i = 0; i < (dimension ** 2); i++) {
+        let newCell = document.createElement("div");
+        newCell.classList.add("cell");
+        container.appendChild(newCell);
     }
 }
 
-makeRows(16)
+makeGrid(60);
 
-function hoverCell(event) {
-    this.style.backgroundColor = "black";
-}
-
-const cellsGrid = document.querySelectorAll(".cell");
-cellsGrid.forEach((cellGrid) => {
-    cellGrid.addEventListener("mouseover", hoverCell);
+const myCells = document.querySelectorAll(".cell");
+myCells.forEach((cell) => {
+    cell.addEventListener("mouseover", changeBackgroundColor)
 })
 
-const gridButton = document.querySelector("#new-grid");
-gridButton.addEventListener("click", newGrid)
+function changeBackgroundColor() {
+    this.style.backgroundColor = "black";
+}
